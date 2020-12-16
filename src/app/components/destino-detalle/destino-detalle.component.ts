26d403a1b586e6f00ca0f357957d62f1475e18d2
Adrian Holzer <adri.holzer.e@gmail.com>
2020-12-16 00:42:37 -0300

    proyecto angular modulo 3

---
+++ src/app/components/destino-detalle/destino-detalle.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, InjectionToken, Inject } from '@angular/core';
+import { DestinosApiClient } from './../../models/destinos-api-client.model';
+import { DestinoViaje } from './../../models/destino-viaje.model';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-destino-detalle',
+  templateUrl: './destino-detalle.component.html',
+  styleUrls: ['./destino-detalle.component.css'],
+  providers: [DestinosApiClient]
+})
+
+export class DestinoDetalleComponent implements OnInit {
+  destino: DestinoViaje;
+  style = {
+    sources: {
+      world: {
+        type: 'geojson',
+        data:
+          'https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json',
+      },
+    },
+    version: 8,
+    layers: [
+      {
+        id: 'countries',
+        type: 'fill',
+        source: 'world',
+        layout: {},
+        paint: {
+          'fill-color': '#6F788A',
+        },
+      },
+    ],
+  };
+
+  constructor(public route: ActivatedRoute, public destinosApiClient: DestinosApiClient) {}
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.destino = this.destinosApiClient.getById(id);
+  }
+
+}
